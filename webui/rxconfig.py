c36abcd85408ce3b37e115f2d5469f492b2eda5f
Van Nt <vannt.sptb@gmail.com>
2023-09-28 15:41:42 +0700

    Update rxconfig.py

--- webui/rxconfig.py
+++ webui/rxconfig.py
@@ -9,7 +9,7 @@ config = WebuiConfig(
     app_name="webui",
     db_url="sqlite:///reflex.db",
     api_url="https://reflex-demo.onrender.com:8000",
-    env=rx.Env.DEV,
+    # env=rx.Env.DEV,
     frontend_packages=[
         "react-loading-icons",
     ],
